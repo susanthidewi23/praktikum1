5cc34b1fd1222645927a9d7459137145d4441c53
susanthidewi23 <susanthidewi2303@gmail.com>
2024-05-17 01:42:35 +0800

    first commit

---
+++ kondisi1.php
@@ -0,0 +1,35 @@
+<?php
+// $nilai = 80;
+// if ($nilai >= 60) {
+// echo "Anda Lulus dengan Nilai $nilai";
+// }
+?>
+
+
+<?php 
+// $nilai = 80;
+// if ($nilai >=60){
+//     echo "Anda Lulus dengan Nilai $nilai";
+// } 
+?> -->
+
+<?php
+// $nilai = 50;
+// if ($nilai >= 60) {
+//     echo "Anda Lulus dengan Nilai $nilai";
+// } else {
+//     echo "Anda Tidak Lulus dengan $nilai";
+// }
+?>
+
+
+<?php
+$nilai = 90;
+while ($nilai <=100) {
+    if ($nilai <=50){
+        echo"nilai sangat kurang" . $nilai. "<br>";
+    } else{
+        echo "Anda tidak lulus ujian dengan nilai $nilai"
+    }
+}
+
